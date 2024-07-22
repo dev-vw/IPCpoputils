6b5b7c58713b1445a564044ebeb1407018667a73
dev-vw <wang.vania@gmail.com>
2024-07-22 09:20:12 +0200

    add scratch script

--- tests/_scratch.R
+++ tests/_scratch.R
@@ -1,7 +1,7 @@
 pop_data <- zambia
-pop_sf_zb <- zambia_adm2
+pop_sf_zb <- zambia_adm4
 
-joined_df <- join_data_to_shp(pop_data, pop_sf_zb, adm_level = "ADM2", yr = 2015)
+joined_df <- join_data_to_shp(pop_data, pop_sf_zb, adm_level = "ADM4", yr = 2015)
 
 plot_chlor(joined_df,
            "Zambia",
